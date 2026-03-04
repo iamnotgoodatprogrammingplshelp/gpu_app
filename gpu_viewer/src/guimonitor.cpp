4dd02b67d2f475e4ee8feadd0f07433256029b15
iamnotgoodatprogrammingplshelp <88812142+iamnotgoodatprogrammingplshelp@users.noreply.github.com>
2026-03-04 23:52:13 +0200

    Add GUI components to GuiMonitor class
    Implemented GUI components for energy monitoring and logging.

--- gpu_viewer/src/guimonitor.cpp
+++ gpu_viewer/src/guimonitor.cpp
@@ -1,28 +1,62 @@
-#ifndef GUIMONITOR_H
-#define GUIMONITOR_H
+#include "guimonitor.h"
+#include <QVBoxLayout>
+#include <QStringList>
+#include <QTime>
+#include <QRandomGenerator>
 
-#include <QWidget>
-#include <QTimer>
-#include <QTextEdit>
-#include <QProgressBar>
+GuiMonitor::GuiMonitor(QWidget *parent) : QWidget(parent), cycle(0), energy(100) {
+    QVBoxLayout *layout = new QVBoxLayout(this);
+    
+    energyLevel = new QProgressBar(this);
+    energyLevel->setRange(0, 100);
+    energyLevel->setValue(energy);
+    energyLevel->setFormat("Energy Level: %p%");
+    
+    thoughtLog = new QTextEdit(this);
+    thoughtLog->setReadOnly(true);
+    
+    layout->addWidget(energyLevel);
+    layout->addWidget(thoughtLog);
 
-class GuiMonitor : public QWidget {
-    Q_OBJECT
-public:
-    explicit GuiMonitor(QWidget *parent = nullptr);
+    heartbeat = new QTimer(this);
+    connect(heartbeat, &QTimer::timeout, this, &GuiMonitor::updateVitals);
+    heartbeat->start(2500);
+}
 
-public slots:
-    void react();
+void GuiMonitor::react() {
+    thoughtLog->setTextColor(QColor(Qt::red));
+    thoughtLog->append(QTime::currentTime().toString("hh:mm:ss") + " - Sudden external stimulus detected. Heart rate spiking.");
+    thoughtLog->setTextColor(QColor(Qt::black));
+    
+    heartbeat->setInterval(800);
+    energy -= 10;
+    if (energy < 0) energy = 0;
+    energyLevel->setValue(energy);
+    
+    QTimer::singleShot(6000, [this]() { 
+        heartbeat->setInterval(2500);
+        thoughtLog->append(QTime::currentTime().toString("hh:mm:ss") + " - Heart rate stabilizing. Returning to baseline.");
+    });
+}
 
-private slots:
-    void updateVitals();
-
-private:
-    QTextEdit *thoughtLog;
-    QProgressBar *energyLevel;
-    QTimer *heartbeat;
-    int cycle;
-    int energy;
-};
-
-#endif
+void GuiMonitor::updateVitals() {
+    QStringList states = {
+        "Regulating internal temperature...",
+        "Scanning environment for changes...",
+        "Processing auditory input...",
+        "Consolidating short-term memory...",
+        "Maintaining balance...",
+        "Experiencing a fleeting random thought..."
+    };
+    
+    int index = QRandomGenerator::global()->bounded(states.size());
+    thoughtLog->append(QTime::currentTime().toString("hh:mm:ss") + " - " + states.at(index));
+    
+    if (energy < 100 && heartbeat->interval() == 2500) {
+        energy += 2;
+        if (energy > 100) energy = 100;
+        energyLevel->setValue(energy);
+    }
+    
+    cycle++;
+}
